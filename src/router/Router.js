c55b5f684793f23359bce5f23344d8cc54402591
Burak Köksal <burak.koksal@teknosa.com>
2023-09-09 14:17:18 +0300

    first commit

---
+++ src/router/Router.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+//screen
+import HomeScreen from '../screens/Home/HomeScreen';
+
+
+const Router = () => {
+  return (
+    <HomeScreen />
+  );
+};
+
+export default Router;
